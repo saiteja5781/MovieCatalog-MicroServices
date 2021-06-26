e99c38733e87d8eded26af35d0e80d840f4dd9fd
Sai Teja <Sai Teja@DESKTOP-N2VQBHK>
2021-06-26 13:00:43 +0530

    initial commit for movie ratings.

---
+++ microservices-movieratings/src/main/java/com/vm/movieratings/MicroservicesMovieratingsApplication.java
@@ -0,0 +1,13 @@
+package com.vm.movieratings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesMovieratingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesMovieratingsApplication.class, args);
+	}
+
+}
---
+++ microservices-movieratings/src/main/java/com/vm/movieratings/resources/RatingsResource.java
@@ -0,0 +1,19 @@
+package com.vm.movieratings.resources;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vm.movieratings.model.Rating;
+
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingsResource {
+
+	@RequestMapping("/{movieId}")
+	public Rating getRating(@PathVariable("movieId") String movieId) {
+		return new Rating("movieid1", 1);
+	}
+
+}
