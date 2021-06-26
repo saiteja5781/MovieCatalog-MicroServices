6c9ce63e120d9e41f9904bbaf2501119a242854c
Sai Teja <Sai Teja@DESKTOP-N2VQBHK>
2021-06-26 14:18:41 +0530

    hardcoded data --mocked ratings call and mocked movie info call

--- microservices-moviecatalog/src/main/java/com/vm/moviecatalog/resources/MovieCatalogResource.java
+++ microservices-moviecatalog/src/main/java/com/vm/moviecatalog/resources/MovieCatalogResource.java
@@ -2,12 +2,14 @@ package com.vm.moviecatalog.resources;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vm.moviecatalog.model.CatalogItem;
+import com.vm.moviecatalog.model.Rating;
 
 
 @RestController
@@ -17,12 +19,17 @@ public class MovieCatalogResource {
 
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-		return Arrays.asList(
-				new CatalogItem("firstmovie", "first movie desc", 1),
-				new CatalogItem("secondmovie", "second movie desc", 2)
-
+		List<Rating> ratings = Arrays.asList(
+				
+				new Rating("movieOne",1),
+				new Rating("movieTwo",2),
+				new Rating("movieThree",3)
 				);
 
+		
+		return ratings.stream()                                                  //putting the list of items [emptying the bag on a] conveyor belt
+				.map(rating -> new CatalogItem(rating.getMovieId(), "description of "+ rating.getMovieId(), rating.getRating()))   //converting each rating item moving on the conveyor belt TO  catalogitem
+				.collect(Collectors.toList());
 	}
 
 }
